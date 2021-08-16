a8b7db68ecefd02d05a100181c61638833c3d6db
kyubin <jikyubin72@gmail.com>
2021-08-16 21:03:29 +0900

    add demo owner API

--- routes/owners.js
+++ routes/owners.js
@@ -1,35 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { Receipt, User } = require('../models')
-const consts = require('../public/javascripts/consts')
+const { Receipt, User } = require('../models');
+const consts = require('../public/javascripts/consts');
+const ownerData = require('../config/ownerData.json');
 
 router.get('/', function (req, res, next) {
     // 유저의 객체를 받음
     user = req.body.user
 
-    // pagination
-    let pageNum = req.query.pageNum || 1;
-    let pageSize = req.query.pageSize || 10;
-    let offset = 0
-    if (pageNum > 1) {
-        offset = pageSize * (pageSize - 1);
-    }
-
     if(user.userCategory = consts.OWNER_CATEGORY){
-        User.findOne({attributes: ['id'], where: { phoneNumber: user.phoneNumber }})
-        .then(seller => {
-            if(seller) 
-            {
-                Receipt.findAll({ where: { seller: seller }, order: [['orderDate', 'DESC']], offset: offset, limit: pageSize })
-                .then(receipts => res.json({ results: receipts }))
-                .catch(err => { res.status(400).json({ error: err.message }) })
-            }
-            else res.json(400).json({error:"점주가 아님"})
-        })
-        .catch(err => {
-            console.log(err.message)
-            res.status(500).json({error:err.message})
-          })
+        res.json({result:ownerData})
     }
     else{res.status(400).json({error:"점주가 아님"})}
   })
